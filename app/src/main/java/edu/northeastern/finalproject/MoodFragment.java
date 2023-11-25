0addef0d14f67d20d7bf64247533956fa63e5208
Rolin128 <li.yuchen@northeastern.edu>
2023-11-24 22:34:50 -0800

    Yuchen-Modified and improved user interface

--- app/src/main/java/edu/northeastern/finalproject/MoodFragment.java
+++ app/src/main/java/edu/northeastern/finalproject/MoodFragment.java
@@ -1,19 +1,31 @@
 package edu.northeastern.finalproject;
 
-import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SeekBar;
-import android.widget.TextView;
+import java.text.DateFormatSymbols;
+
 
 public class MoodFragment extends Fragment {
 
     private SeekBar moodSeekBar;
-    private TextView moodTextGood, moodTextAverage, moodTextBad;
+    private ImageView moodImageGood, moodImageAverage, moodImageBad;
+
+    private TextView moodValueText;
 
     public MoodFragment() {
         // Required empty public constructor
@@ -28,14 +40,20 @@ public class MoodFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mood, container, false);
         moodSeekBar = view.findViewById(R.id.moodSeekBar);
-        moodTextGood = view.findViewById(R.id.moodTextGood);
-        moodTextAverage = view.findViewById(R.id.moodTextAverage);
-        moodTextBad = view.findViewById(R.id.moodTextBad);
+        moodImageGood = view.findViewById(R.id.moodImageGood);
+        moodImageAverage = view.findViewById(R.id.moodImageAverage);
+        moodImageBad = view.findViewById(R.id.moodImageBad);
+        moodValueText = view.findViewById(R.id.moodValueText);
+
+        moodImageGood.setImageResource(R.drawable.ic_mood_good);
+        moodImageAverage.setImageResource(R.drawable.ic_mood_average);
+        moodImageBad.setImageResource(R.drawable.ic_mood_bad);
 
         moodSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                int moodValue = seekBar.getProgress();
+                moodValueText.setText(String.valueOf(moodValue));
             }
 
             @Override
@@ -45,10 +63,56 @@ public class MoodFragment extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                // No action needed
+                int moodValue = seekBar.getProgress();
+                moodValueText.setText(String.valueOf(moodValue));
+
+                new AlertDialog.Builder(getContext())
+                        .setMessage("Do you want to save mood？")
+                        .setPositiveButton("save", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                saveMoodValue(moodValue);
+                                Toast.makeText(getContext(), "save successfully!", Toast.LENGTH_SHORT).show(); //show saved info
+                            }
+                        })
+                        .setNegativeButton("cancel", null)
+                        .show();
+
             }
+
+
         });
 
         return view;
     }
+    private void saveMoodValue(int moodValue) {
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String currentDate = dateFormat.format(calendar.getTime());
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        String dayOfWeekStr = new DateFormatSymbols().getWeekdays()[dayOfWeek];
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("MoodPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+
+        editor.putInt("MoodValue", moodValue);
+        editor.putString("Date", currentDate);
+        editor.putString("DayOfWeek", dayOfWeekStr);
+        editor.apply();
+
+
+        showSavedMoodData(currentDate, dayOfWeekStr, moodValue);
+    }
+
+    private void showSavedMoodData(String date, String dayOfWeek, int moodValue) {
+
+        String message = String.format(Locale.getDefault(), "%s, %s, %d", date, dayOfWeek, moodValue);
+
+
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+
 }
+
