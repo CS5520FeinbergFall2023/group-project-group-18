ddbf9b8a65405193977f3a3a3a1353c4dea76004
Rolin128 <li.yuchen@northeastern.edu>
2023-12-01 23:16:20 -0800

    Show mood color on month calendar

--- app/src/main/java/edu/northeastern/finalproject/MoodFragment/CustomMonthView.java
+++ app/src/main/java/edu/northeastern/finalproject/MoodFragment/CustomMonthView.java
@@ -7,24 +7,49 @@ import android.graphics.Color;
 import com.haibin.calendarview.MonthView;
 import com.haibin.calendarview.Calendar;
 
+
 public class CustomMonthView extends MonthView {
+
+    private int mRadius;
 
     public CustomMonthView(Context context) {
         super(context);
     }
 
     @Override
-    protected void onDrawText(Canvas canvas, Calendar calendar, int x, int y, boolean hasScheme, boolean isSelected) {
-        canvas.drawText(calendar.getDay() + "", x + mItemWidth / 2, mTextBaseLine + y - mItemHeight / 6, calendar.isCurrentDay() ? mCurDayTextPaint : mCurMonthTextPaint);
-    }
-
-    @Override
-    protected void onDrawScheme(Canvas canvas, Calendar calendar, int x, int y) {
-
+    protected void onPreviewHook() {
+        mRadius = Math.min(mItemWidth, mItemHeight) / 5 * 2;
     }
 
     @Override
     protected boolean onDrawSelected(Canvas canvas, Calendar calendar, int x, int y, boolean hasScheme) {
         return true;
+    }
+
+    @Override
+    protected void onDrawScheme(Canvas canvas, Calendar calendar, int x, int y) {
+        int cx = x + mItemWidth / 2;
+        int cy = y + mItemHeight / 2;
+        canvas.drawCircle(cx, cy, mRadius, mSchemePaint);
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    @Override
+    protected void onDrawText(Canvas canvas, Calendar calendar, int x, int y, boolean hasScheme, boolean isSelected) {
+        int cx = x + mItemWidth / 2;
+        int top = y - mItemHeight / 8;
+        if (isSelected) {
+            canvas.drawText(String.valueOf(calendar.getDay()), cx, mTextBaseLine + top,
+                    calendar.isCurrentDay() ? mCurDayTextPaint : mSelectTextPaint);
+        } else if (hasScheme) {
+            canvas.drawText(String.valueOf(calendar.getDay()), cx, mTextBaseLine + top,
+                    calendar.isCurrentDay() ? mCurDayTextPaint :
+                            calendar.isCurrentMonth() ? mSchemeTextPaint : mOtherMonthTextPaint);
+
+        } else {
+            canvas.drawText(String.valueOf(calendar.getDay()), cx, mTextBaseLine + top,
+                    calendar.isCurrentDay() ? mCurDayTextPaint :
+                            calendar.isCurrentMonth() ? mCurMonthTextPaint : mOtherMonthTextPaint);
+        }
     }
 }
