d3cb67f1289781d2619049da7a3a67513b7f3d2b
Manhong Yuan <Yuan.Manh@northeastern.edu>
2023-12-05 21:51:57 -0500

    add enter daily quote in addMood page, quote can be uploaded to firestore

--- app/src/main/java/edu/northeastern/finalproject/data/UserDailyRecord.java
+++ app/src/main/java/edu/northeastern/finalproject/data/UserDailyRecord.java
@@ -13,18 +13,29 @@ public class UserDailyRecord {
     private int steps;
     private double heartRate;
 
+    public String getDailyQuote() {
+        return dailyQuote;
+    }
+
+    public void setDailyQuote(String dailyQuote) {
+        this.dailyQuote = dailyQuote;
+    }
+
+    private String dailyQuote;
+
     // Default constructor is needed for Firestore's automatic data mapping
     public UserDailyRecord() {
     }
 
     // Constructor with parameters
-    public UserDailyRecord(String userId, Date date, Integer mood, List<String> photoUrls, int steps, double heartRate) {
+    public UserDailyRecord(String userId, Date date, Integer mood, List<String> photoUrls, int steps, double heartRate, String dailyQuote) {
         this.userId = userId;
         this.date = date;
         this.mood = mood;
         this.photoUrls = photoUrls;
         this.steps = steps;
         this.heartRate = heartRate;
+        this.dailyQuote = dailyQuote;
     }
 
     // Getters and setters for each field for Firestore's automatic data mapping
@@ -82,7 +93,6 @@ public class UserDailyRecord {
                 "userId='" + userId + '\'' +
                 ", date=" + date +
                 ", photoUrls=" + photoUrls +
-                ", ... other fields ..." +
-                '}';
+                ", dailyQuote=" + dailyQuote;
     }
 }
